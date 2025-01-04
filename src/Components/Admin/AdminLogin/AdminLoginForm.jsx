c406c4fc5630d019cabc83d8f6a809ed5511d17c
seema <seemarughade1363@gmail.com>
2025-01-04 19:29:06 +0530

    new latest code

--- src/Components/Admin/AdminLogin/AdminLoginForm.jsx
+++ src/Components/Admin/AdminLogin/AdminLoginForm.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {useNavigate } from 'react-router-dom';
 
 const AdminLoginForm = () => {
+    const navigate = useNavigate();
+  
   const [formData, setFormData] = useState({
     username: '',
     password: '',
